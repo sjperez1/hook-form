70c19ab34dbd74aa3870510f4931e153e5c58e81
sjperez1 <sarahpz479@gmail.com>
2022-07-08 13:29:43 -0600

    updated form

--- src/components/HookForm.jsx
+++ src/components/HookForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from  'react';
+import DisplayForm from './DisplayForm';
 
 const UserForm = () => {
     const [firstname, setFirstname] = useState("");
@@ -18,34 +19,27 @@ const UserForm = () => {
             <form onSubmit={createNewUser}>
                 <div>
                     <label>First Name </label> 
-                    <input type="text" onChange={(e) => setFirstname(e.target.value)} />
+                    <input type="text" value={firstname} onChange={(e) => setFirstname(e.target.value)} />
                 </div>
                 <div>
                     <label>Last Name </label> 
-                    <input type="text" onChange={(e) => setLastname(e.target.value)} />
+                    <input type="text" value={lastname} onChange={(e) => setLastname(e.target.value)} />
                 </div>
                 <div>
                     <label>Email </label> 
-                    <input type="text" onChange={(e) => setEmail(e.target.value)} />
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div>
                     <label>Password </label>
-                    <input type="text" onChange={(e) => setPassword(e.target.value)} />
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div>
                     <label>Confirm Password </label>
-                    <input type="text" onChange={(e) => setConfirmpassword(e.target.value)} />
+                    <input type="password" value={confirmpassword} onChange={(e) => setConfirmpassword(e.target.value)} />
                 </div>
                 <input type="submit" value="Create User" />
             </form>
-            <div>
-                <h3>Your Form Data</h3>
-                <p>First Name {firstname}</p>
-                <p>Last Name {lastname}</p>
-                <p>Email {email}</p>
-                <p>Password {password}</p>
-                <p>Confirm Password {confirmpassword}</p>
-            </div>
+            <DisplayForm firstname={firstname} lastname={lastname} email={email} password={password} confirmpassword={confirmpassword}/>
         </div>
     );
 };
